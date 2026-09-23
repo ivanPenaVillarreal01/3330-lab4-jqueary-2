58bee8932500354a9f91e7b57a4263a58cb3ea3c
Ivan Pena <ivan.penavillarreal01@utrgv.edu>
2026-09-23 17:04:50 -0500

    Update game list

--- js/example.js
+++ js/example.js
@@ -2,7 +2,28 @@ $(function() {
   
   // TODO: Create a list of JS Object each representing a game. 
   // Use the data contained in the <ul> to build this list of game titles.
+const games = [
+  {
+    "title":  "The Legend of Zelda: Breath of the Wild"
+  },
 
+  
+  {
+   "title": "God of War Ragnarök"
+  },
+
+  {
+    "title": "Halo Infinite"
+  },
+
+  {
+    "title": "Minecraft"
+  },
+
+  {
+    "title": "Super Mario Odyssey"
+  }
+];
 
   var gameList, newItemForm, newItemButton;
   var item = '';                                 
@@ -16,7 +37,18 @@ $(function() {
   // To do so, create a function that loops through each object in the game list, 
   // create a new node "list item" holding the game title and 
   // inject the new node inside the <ul>.
+function loadGames(){
+  games.forEach(function(game) {
 
+  const listItem = $("<li>");
+
+  listItem.html(game.title);
+
+   gameList.append(listItem);
+   });
+
+}
+loadGames();
 
 
   function updateCount() {                      
